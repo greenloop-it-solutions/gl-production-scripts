2664c1e6c02777eb486d135fb1b1dfe15d6ac70a
smood922 <48338396+smood922@users.noreply.github.com>
2022-03-09 08:28:28 -0800

    Update Create-StandardRetentionPolicies.ps1
    Updated to read-host the admin UPN name.

--- Microsoft 365/Create-StandardRetentionPolicies.ps1
+++ Microsoft 365/Create-StandardRetentionPolicies.ps1
@@ -1,8 +1,10 @@
 #Script Creates a Retention Policy for Exchange/OneDrive/SharePoint etc with 7 year/Keep retention, and another for Teams Channels and Chats. Teams private channel messages are currently only supported via the GUI (https://compliance.microsoft.com/informationgovernance?viewid=retention) not PowerShell.
-#fill in the admin UPN and run in PowerShell. EXOv2 should already be loaded.
+#EXOv2 will need to already be loaded.
+
+$adminUpn = Read-Host "provide the admin UPN for the tenant"
 
 Import-Module ExchangeOnlineManagement
-Connect-IPPSSession -UserPrincipalName <Admin UPN goes here>
+Connect-IPPSSession -UserPrincipalName $adminUpn
 
 New-RetentionCompliancePolicy -Name "Company Policy" -ExchangeLocation All -SharePointLocation All -ModernGroupLocation All -OneDriveLocation All -PublicFolderLocation All -Enabled $true
 New-RetentionComplianceRule -Name "Company Policy Rule" -Policy "Company Policy" -RetentionDuration 2555 -ExpirationDateOption CreationAgeInDays -RetentionComplianceAction Keep   
