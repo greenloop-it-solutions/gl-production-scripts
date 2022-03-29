0e3eaa1994c338c11da2e2a3474934fbde8aff20
smood922 <48338396+smood922@users.noreply.github.com>
2022-03-29 10:42:14 -0700

    Create Add-AudioConferencingSKUforAllUsers.ps1
    initial commit

---
+++ Microsoft 365/Add-AudioConferencingSKUforAllUsers.ps1
@@ -0,0 +1,187 @@
+# adds Microsoft Teams Audio Conferencing Select Dial out to the users in a CSV (the CSV should be exported from Admin center, edited to remove unneeded users and then selected when prompted).
+
+#function Get-FileName from https://github.com/myusefulrepo/Tips/blob/master/Tips%20%20-%20How%20to%20create%20an%20open%20file%20and%20folder%20dialog%20box%20with%20PowerShell.md
+function Get-FileName
+{
+<#
+.SYNOPSIS
+   Show an Open File Dialog and return the file selected by the user
+
+.DESCRIPTION
+   Show an Open File Dialog and return the file selected by the user
+
+.PARAMETER WindowTitle
+   Message Box title
+   Mandatory - [String]
+
+.PARAMETER InitialDirectory
+   Initial Directory for browsing
+   Mandatory - [string]
+
+.PARAMETER Filter
+   Filter to apply
+   Optional - [string]
+
+.PARAMETER AllowMultiSelect
+   Allow multi file selection
+   Optional - switch
+
+ .EXAMPLE
+   Get-FileName
+    cmdlet Get-FileName at position 1 of the command pipeline
+    Provide values for the following parameters:
+    WindowTitle: My Dialog Box
+    InitialDirectory: c:\temp
+    C:\Temp\42258.txt
+
+    No passthru paramater then function requires the mandatory parameters (WindowsTitle and InitialDirectory)
+
+.EXAMPLE
+   Get-FileName -WindowTitle MyDialogBox -InitialDirectory c:\temp
+   C:\Temp\41553.txt
+
+   Choose only one file. All files extensions are allowed
+
+.EXAMPLE
+   Get-FileName -WindowTitle MyDialogBox -InitialDirectory c:\temp -AllowMultiSelect
+   C:\Temp\8544.txt
+   C:\Temp\42258.txt
+
+   Choose multiple files. All files are allowed
+
+.EXAMPLE
+   Get-FileName -WindowTitle MyDialogBox -InitialDirectory c:\temp -AllowMultiSelect -Filter "text file (*.txt) | *.txt"
+   C:\Temp\AES_PASSWORD_FILE.txt
+
+   Choose multiple files but only one specific extension (here : .txt) is allowed
+
+.EXAMPLE
+   Get-FileName -WindowTitle MyDialogBox -InitialDirectory c:\temp -AllowMultiSelect -Filter "Text files (*.txt)|*.txt| csv files (*.csv)|*.csv | log files (*.log) | *.log"
+   C:\Temp\logrobo.log
+   C:\Temp\mylogfile.log
+
+   Choose multiple file with the same extension
+
+.EXAMPLE
+   Get-FileName -WindowTitle MyDialogBox -InitialDirectory c:\temp -AllowMultiSelect -Filter "selected extensions (*.txt, *.log) | *.txt;*.log"
+   C:\Temp\IPAddresses.txt
+   C:\Temp\log.log
+
+   Choose multiple file with different extensions
+   Nota :It's important to have no white space in the extension name if you want to show them
+
+.EXAMPLE
+ Get-Help Get-FileName -Full
+
+.INPUTS
+   System.String
+   System.Management.Automation.SwitchParameter
+
+.OUTPUTS
+   System.String
+
+.NOTESs
+  Version         : 1.0
+  Author          : O. FERRIERE
+  Creation Date   : 11/09/2019
+  Purpose/Change  : Initial development
+
+  Based on different pages :
+   mainly based on https://blog.danskingdom.com/powershell-multi-line-input-box-dialog-open-file-dialog-folder-browser-dialog-input-box-and-message-box/
+   https://code.adonline.id.au/folder-file-browser-dialogues-powershell/
+   https://thomasrayner.ca/open-file-dialog-box-in-powershell/
+#>
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        # WindowsTitle help description
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Message Box Title",
+            Position = 0)]
+        [String]$WindowTitle,
+
+        # InitialDirectory help description
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Initial Directory for browsing",
+            Position = 1)]
+        [String]$InitialDirectory,
+
+        # Filter help description
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Filter to apply",
+            Position = 2)]
+        [String]$Filter = "All files (*.*)|*.*",
+
+        # AllowMultiSelect help description
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Allow multi files selection",
+            Position = 3)]
+        [Switch]$AllowMultiSelect
+    )
+
+    # Load Assembly
+    Add-Type -AssemblyName System.Windows.Forms
+
+    # Open Class
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+
+    # Define Title
+    $OpenFileDialog.Title = $WindowTitle
+
+    # Define Initial Directory
+    if (-Not [String]::IsNullOrWhiteSpace($InitialDirectory))
+    {
+        $OpenFileDialog.InitialDirectory = $InitialDirectory
+    }
+
+    # Define Filter
+    $OpenFileDialog.Filter = $Filter
+
+    # Check If Multi-select if used
+    if ($AllowMultiSelect)
+    {
+        $OpenFileDialog.MultiSelect = $true
+    }
+    $OpenFileDialog.ShowHelp = $true    # Without this line the ShowDialog() function may hang depending on system configuration and running from console vs. ISE.
+    $OpenFileDialog.ShowDialog() | Out-Null
+    if ($AllowMultiSelect)
+    {
+        return $OpenFileDialog.Filenames
+    }
+    else
+    {
+        return $OpenFileDialog.Filename
+    }
+}
+
+#make sure to provide delegated admin creds here!
+Connect-MsolService
+
+do {
+    $tenantdomain = read-host "Enter the client's Office 365 domain name"
+
+    $tenantId = (Get-MsolPartnerContract -DomainName $tenantdomain | Select-Object TenantId).tenantid
+
+    $SKU = (Get-MsolAccountSku -TenantId $tenantId | ? {$_.AccountSKUId -like "*Microsoft_Teams_Audio_Conferencing_select_dial_out"}).AccountSkuId
+
+    $CSVFile = Get-filename -windowTitle "Please select the CSV file containing your user list" -initialdirectory "%userprofile%\downloads"
+
+    $users = import-csv $CSVFile -delimiter ","
+    foreach ($user in $users) {
+        $upn=$user.'user principal name'
+        $usagelocation=$user.'usage location' 
+        Set-MsolUser -UserPrincipalName $upn -UsageLocation $usagelocation -TenantId $tenantId
+        Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $SKU -TenantId $tenantId
+    }
+    $continue = "Would you like to run again with a different tenant? (Y|N)" 
+}
+while ($continue -eq 'Y')
