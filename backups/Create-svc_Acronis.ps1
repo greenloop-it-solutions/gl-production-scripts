b7e46e67d6f04dcb231896e7e58f047800154484
smood922 <48338396+smood922@users.noreply.github.com>
2022-10-24 15:44:13 -0700

    Update Create-svc_Acronis.ps1

--- backups/Create-svc_Acronis.ps1
+++ backups/Create-svc_Acronis.ps1
@@ -8,12 +8,12 @@ function Get-RandomPassword {
     Add-Type -AssemblyName 'System.Web'
     return [System.Web.Security.Membership]::GeneratePassword($length, $amountOfNonAlphanumeric)
 }
-$pw = Get-RandomPassword 10
+$pwPlain = Get-RandomPassword 10
 
-$pw = $pw | ConvertTo-SecureString -AsPlainText -Force
+$pw = $pwPlain | ConvertTo-SecureString -AsPlainText -Force
 New-ADUser -Name svc_Acronis -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Domain Admins" -Members svc_Acronis
 
-Write-Host: "Password: $pw"
+Write-Host "Password: $pwPlain"
 
-Write-Host: "Once agent is installed, update the Acronis Management Service to run as Local System and delete this account!"
+Write-Host "Once agent is installed, update the Acronis Management Service to run as Local System and delete this account!"
