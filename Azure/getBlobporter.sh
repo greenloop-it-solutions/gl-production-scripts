7d63f90fea9c753db3312bc1e1006d363374f199
smood922 <48338396+smood922@users.noreply.github.com>
2022-10-26 10:45:44 -0700

    Create getBlobporter.sh

---
+++ Azure/getBlobporter.sh
@@ -0,0 +1,4 @@
+wget -O bp_linux.tar.gz https://github.com/Azure/blobporter/releases/download/v0.6.20/bp_linux.tar.gz
+tar -xvf bp_linux.tar.gz linux_amd64/blobporter
+chmod +x ~/linux_amd64/blobporter
+cd ~/linux_amd64
